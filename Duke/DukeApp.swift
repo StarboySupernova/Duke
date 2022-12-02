06b9ceb1206d249fb58877d463570d41e9f2441f
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2022-12-02 23:22:45 +0100

    Initial Commit

---
+++ Duke/DukeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DukeApp.swift
+//  Duke
+//
+//  Created by user226714 on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct DukeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
