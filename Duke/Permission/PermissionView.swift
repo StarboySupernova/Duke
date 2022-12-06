679274ed32719e459616227b42b1f217f3376bf9
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2022-12-06 18:26:05 +0100

    Added basic structure & animations to PermissionView

--- Duke/Permission/PermissionView.swift
+++ Duke/Permission/PermissionView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct PermissionView: View {
+    @State private var isAnimating: Bool = false
+    
+    var animation : Animation {
+        .interpolatingSpring(stiffness: 0.5, damping: 0.5)
+        .delay(isAnimating ? .random(in: 0...1) : 0)
+        .speed(isAnimating ? .random(in: 0.3...1) : 0)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            VStack {
+                ZStack {
+                    ForEach(1..<14) { index in
+                        Image("food\(index % 7)")
+                        //self positioning through geometry bounds
+                            .position(x: .random(in: 0...geometry.size.width),
+                                      y: .random(in: 0...geometry.size.height / 2)
+                            )
+                            .animation(animation, value: isAnimating)
+                    }
+                }
+                .frame(height: geometry.size.height / 3)
+                
+                Text("Duke")
+                    .font(.largeTitle)
+                    .fontWeight(.ultraLight)
+                
+                Text("Event Search Companion App")
+                    .font(.headline)
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("Get Started")
+                        .bold()
+                }
+                .padding()
+                .frame(width: geometry.size.width - 30)
+                .background(Color.black)
+                .cornerRadius(15)
+                .shadow(radius: 10)
+                .foregroundColor(.white)
+            }
+        }
+        .onAppear {
+            isAnimating.toggle()
+        }
     }
 }
 
