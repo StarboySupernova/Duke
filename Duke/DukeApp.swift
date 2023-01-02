a6f06a380bf174ca2e679d25626cdf98b7d93c7a
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-01-02 13:31:19 +0100

    Verbose Firebase init & AppDelegate used to leave room to implement OTP phone auth in future

--- Duke/DukeApp.swift
+++ Duke/DukeApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseStorage
 
 @main
 struct DukeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     init() {
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = .black
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).tintColor = .white
@@ -20,5 +27,22 @@ struct DukeApp: App {
                 .modifier(DarkModeViewModifier())
                 .environmentObject(HomeViewModel())
         }
+    }
+}
+
+//MARK: Used more verbose Firebase setup to enable OTP phone authentication in future
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.all //By default we want all our views to rotate freely
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        //needed for phone Auth
+    }
+        
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
