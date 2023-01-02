e8bc2f45561417686175e404336ada224c4f820d
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-01-02 09:27:59 +0100

    ContainerView to house user dashboard in Tabbed form

---
+++ Duke/Views/Login/LoginContainerView.swift
@@ -0,0 +1,39 @@
+//
+//  LoginContainerView.swift
+//  Duke
+//
+//  Created by Simbarashe Dombodzvuku on 1/1/23.
+//
+
+import SwiftUI
+
+struct LoginContainerView: View {
+    @EnvironmentObject var userLoginVM: UserViewModel
+    var body: some View {
+        if userLoginVM.sign_in_status {
+            //MARK: Tab View with Profile & Recent Posts
+            TabView {
+                PostsView()
+                    .tabItem {
+                        Image(systemName: "rectangle.portrait.on.rectangle.portrait.angled")
+                        Text("Posts")
+                    }
+                
+                ProfileView()
+                    .tabItem {
+                        Image(systemName: "gear")
+                        Text("Profile")
+                    }
+            }
+            .tint(.black)
+        } else {
+            LoginView()
+        }
+    }
+}
+
+struct LoginContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginContainerView()
+    }
+}
