cdda371a4ebdfe6dba9771df553d9cadd56c568e
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-01-02 12:24:09 +0100

    Redundant code reducer views ReusablePostsView & UserPostCardView

---
+++ Duke/Views/User Dashboard/ReusableSubViews/ReusablePostsView.swift
@@ -0,0 +1,132 @@
+//
+//  ReusablePostsView.swift
+//  Duke
+//
+//  Created by Simbarashe Dombodzvuku on 1/2/23.
+//
+
+import SwiftUI
+import Firebase
+
+/// To eliminate redundant code since we have to display the current user's posts on screen and display another user's posts when searching for that user
+struct ReusablePostsView: View {
+    var basedOnUID: Bool = false
+    var uid: String = ""
+    @State var isFetching: Bool = false
+    @Binding var posts : [Post]
+    ///Pagination
+    @State private var paginationDoc: QueryDocumentSnapshot?
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVStack { //LazyVStack allows us to receive notifications via onAppear and onDisappear when child views move outside the screen
+                if isFetching {
+                    ProgressView()
+                        .padding(.top, .xxLarge)
+                } else {
+                    if posts.isEmpty {
+                        Text("No Posts Found")
+                            .font(.callout)
+                            .foregroundColor(.gray)
+                            .padding(.top, .xxLarge)
+                    } else {
+                        Posts()
+                    }
+                }
+            }
+            .padding(.large)
+        }
+        .refreshable {
+            //refreshing for posts fetched by UID not supported
+            guard !basedOnUID else { return }
+            isFetching = true
+            posts = []
+            //resetting pagination doc for refreshes since refresh will fetch most recent posts, hence paginationdoc needs to be updated
+            paginationDoc = nil
+            await fetchPosts()
+        }
+        .task {
+            //running fetch operation only when internalPosts has no values
+            guard posts.isEmpty else  { return }
+            await fetchPosts()
+        }
+    }
+    
+    /// Displayiing fetched posts
+    @ViewBuilder func Posts() -> some View {
+        ForEach(posts) { post in
+            UserPostCardView(post: post) { updatedPost in
+                //updating post in array
+                if let index = posts.firstIndex(where: { post in
+                    post.id == updatedPost.id
+                }) {
+                    posts[index].upvoteIDs = updatedPost.upvoteIDs
+                    posts[index].downvoteIDs = updatedPost.downvoteIDs
+                }
+            } onDelete: {
+                //removing post from array
+                withAnimation(.easeInOut(duration: 0.25)) {
+                    posts.removeAll {
+                        post.id == $0.id
+                    }
+                }
+            }
+            .onAppear {
+                //MARK: we null-check pagination here as well, so that when each batch fetch is completed, we will no longer run fetch operation when last document has been reached. This is an infinite scroll feature, which will not overwhelm our db capabilities with I/O operations. Fetch operations run only after user scrolls to the end of previous batch
+                if post.id == posts.last?.id && paginationDoc != nil {
+                    Task {
+                        await fetchPosts()
+                    }
+                }
+            }
+
+            Divider()
+                .padding(.horizontal, -15)
+        }
+    }
+    
+    //should find a way to use this while following MVVM pattern
+    
+    /// asynchronously fetching posts
+    /// will run fetch operation for recent posts, or for a specific userUID based on the Boolean value of basedOnUID
+    func fetchPosts() async {
+        do {
+            var query: Query!
+            ///implementing pagination
+            if let paginationDoc {
+                query = Firestore.firestore().collection("Posts")
+                    .order(by: "publishedDate", descending: true)
+                    .start(afterDocument: paginationDoc)
+                    .limit(to: 20)
+            } else {
+                query = Firestore.firestore().collection("Posts")
+                    .order(by: "publishedDate", descending: true)
+                    .limit(to: 20)
+            }
+            
+            /// new query for UID based fetch operation, by filtering out posts not attached to the passed-in UID
+            if basedOnUID {
+                query = query.whereField("userUID", isEqualTo: uid)
+            }
+            
+            let docs = try await query.getDocuments()
+            let fetchedPosts = try docs.documents.compactMap { document -> Post in
+                try document.data(as: Post.self)
+            } //iffy
+            
+            //saving last fetched dco so that it can be used for pagination in Firestore
+            await MainActor.run(body: {
+                posts.append(contentsOf: fetchedPosts)
+                paginationDoc = docs.documents.last
+                isFetching = false
+            })
+        } catch {
+            showErrorAlertView("Error", error.localizedDescription, handler: {})
+        }
+    }
+}
+
+struct ReusablePostsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostsView()
+    }
+}
---
+++ Duke/Views/User Dashboard/ReusableSubViews/UserPostCardView.swift
@@ -0,0 +1,219 @@
+//
+//  UserPostCardView.swift
+//  Duke
+//
+//  Created by Simbarashe Dombodzvuku on 1/2/23.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+import Firebase
+import FirebaseStorage
+
+struct UserPostCardView: View {
+    ///for live updates
+    @State private var docListener: ListenerRegistration?
+    var post: Post
+    ///callbacks
+    var onUpdate: (Post) -> ()
+    var onDelete: () -> ()
+    var body: some View {
+        HStack(alignment: .top, spacing: .medium) {
+            Image(uiImage: /*(status) ?*/ userProvidedImage("user_Image") ?? UIImage.init(named: "DEMTLogo")! /*: UIImage.init(named: "usericon")!*/)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 35, height: 35)
+                .cornerRadius(10)
+                .padding(.top, .large)
+            
+            VStack(alignment: .leading, spacing: .small) {
+                Text(post.userName)
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                Text(post.publishedDate.formatted(date: .numeric, time: .shortened)) //add condition check to make this .standard if one week elapses
+                    .font(.caption2)
+                    .foregroundColor(.gray)
+                Text(post.text)
+                    .textSelection(.enabled)
+                    .padding(.vertical, .medium)
+                
+                if let postImageURL = post.imageURL {
+                    GeometryReader{ geometry in
+                        let size = geometry.size
+                        WebImage(url: postImageURL)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: size.width, height: size.height)
+                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    }
+                    .frame(height: 200)
+                }
+                
+                InternalPostInteraction()
+            }
+        }
+        .horizontalAlign(.leading)
+        .overlay(alignment: .topTrailing, content: {
+            //delete button for OP
+            if post.userUID == UserDefaults.standard.string(forKey: "user_id")! /*should create helper function to standardise this*/ {
+                Menu {
+                    Button(role: .destructive) {
+                        deletePost()
+                    } label: {
+                        Text("Delete This Post")
+                    }
+
+                } label: {
+                    Image(systemName: "ellipsis")
+                        .font(.caption)
+                        .rotationEffect(.init(degrees: -90))
+                        .foregroundColor(.black)
+                        .padding(.medium)
+                        .contentShape(Rectangle())
+                }
+                .offset(x: .medium)
+            }
+        })
+        .onAppear {
+            //MARK: docListener is added only when post is visible onscreen
+            //MARK: LazyVStack is designed to emit notifications if a child enters or exits screen visibility through onAppear & onDisappear methods respectively
+            ///adding only once
+            if docListener == nil {
+                guard let postID = post.id else {
+                    showErrorAlertView("Error", "Post ID not saved and initialised properly", handler: {})
+                    return
+                }
+                docListener = Firestore.firestore().collection("Posts").document(postID).addSnapshotListener({ snapshot, error in
+                    if let snapshot {
+                        if snapshot.exists {
+                            /// - Document updated
+                            /// fetching updated document
+                            if let updatedPost = try? snapshot.data(as: Post.self) {
+                                onUpdate(updatedPost)
+                            }
+                        } else {
+                           /// - Document deleted
+                            onDelete()
+                        }
+                    }
+                })
+            }
+        }
+        .onDisappear {
+            //MARK: Ensuring that live updates only occur for documents visible on screen to lower I/O cost of Firebase reads
+            //MARK: DocumentListener is removed in such instances
+            if let docListener {
+                docListener.remove()
+                self.docListener = nil
+            }
+        }
+    }
+    
+    //MARK: upvote/downvote
+    @ViewBuilder func InternalPostInteraction() -> some View {
+        HStack(spacing: .small) {
+            Button {
+                likePost()
+            } label: {
+                Image(systemName: post.upvoteIDs.contains(UserDefaults.standard.string(forKey: "user_id")!) ? "hand.thumbsup.fill": "hand.thumbsup")
+            }
+            
+            Text("\(post.upvoteIDs.count)")
+                .font(.caption)
+                .foregroundColor(.gray)
+
+            Button {
+                dislikePost()
+            } label: {
+                Image(systemName: post.downvoteIDs.contains(UserDefaults.standard.string(forKey: "user_id")!) ? "hand.thumbsdown.fill" : "hand.thumbsdown")
+            }
+            .padding(.leading, .xLarge)
+            
+            Text("\(post.downvoteIDs.count)")
+                .font(.caption)
+                .foregroundColor(.gray)
+        }
+        .foregroundColor(.black)
+        .padding(.vertical, .medium)
+    }
+    
+    //should find a way to use this while following MVVM
+    //make these guard conditions at global scope so that these views will not used if the guard fails
+    //should changed to upvote
+    func likePost() {
+        Task {
+            //functionality to allow removing an upvote from a post if it had previously been upvoted
+            guard let postID = post.id else {
+                showErrorAlertView("Error", "Internal-Post ID not saved and initialised properly", handler: {})
+                return
+            }
+            guard let userUID = UserDefaults.standard.string(forKey: "user_id") else {
+                showErrorAlertView("Error", "User ID not found", handler: {})
+                return
+            }
+            if post.upvoteIDs.contains(userUID) {
+                try await Firestore.firestore().collection("Posts").document(postID).updateData([
+                    "upvoteIDs" : FieldValue.arrayRemove([userUID])
+                ])
+            } else {
+                //Adding userUID to upvote array and removing from downvote, if user had made a prior downvote
+                try await Firestore.firestore().collection("Posts").document(postID).updateData([// may have structured my db differently from tut here
+                    "upvoteIDs" : FieldValue.arrayUnion([userUID]),
+                    "downvoteIDs" : FieldValue.arrayRemove([userUID])
+                ])
+            }
+        }
+    }
+    
+    func dislikePost() {
+        Task {
+            //functionality to allow removing an upvote from a post if it had previously been upvoted
+            guard let postID = post.id else {
+                showErrorAlertView("Error", "Post ID not saved and initialised properly", handler: {})
+                return
+            }
+            guard let userUID = UserDefaults.standard.string(forKey: "user_id") else {
+                showErrorAlertView("Error", "User ID not found", handler: {})
+                return
+            }
+            do {
+                if post.downvoteIDs.contains(userUID) {
+                    try await Firestore.firestore().collection("Posts").document(postID).updateData([
+                        "downvoteIDs" : FieldValue.arrayRemove([userUID])
+                    ])
+                } else {
+                    //Adding userUID to downvote array and removing from upvote, if user had made a prior upvote
+                    try await Firestore.firestore().collection("Posts").document(postID).updateData([// may have structured my db differently from tut here
+                        "upvoteIDs" : FieldValue.arrayRemove([userUID]),
+                        "downvoteIDs" : FieldValue.arrayUnion([userUID])
+                    ])
+                }
+            } catch {
+                await setError(error)
+            }
+        }
+    }
+
+    func deletePost() {
+        Task {
+            //step 1 - delete image from firebase storage
+            do {
+                guard let userUID = UserDefaults.standard.string(forKey: "internal_user_id") else {
+                    showErrorAlertView("Error", "Login Initialisation Error", handler: {})
+                    return
+                }
+                if post.imageReferenceID != "" {
+                    try await Storage.storage().reference().child(userUID).child("Social_Images").child(post.imageReferenceID).delete()
+                }
+                //Step 2 - delete firestore document
+                guard let postID = post.id else {
+                    showErrorAlertView("Error", "Something went wrong with initalising this missive", handler: {})
+                    return
+                }
+                try await Firestore.firestore().collection("Internal_Posts").document(postID).delete()
+            } catch {
+                await setError(error)
+            }
+        }
+    }
+}
