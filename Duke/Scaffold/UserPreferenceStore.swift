4827e955cd23014094a9fb4d38411ec3801a612e
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-05-17 13:15:47 +0200

    Renamed UserPreference, changed preference values from String to Bool given the new knowledge that we can query Bool inside a Firestore db

--- Duke/Scaffold/UserPreferenceStore.swift
+++ Duke/Scaffold/UserPreferenceStore.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Preference: ObservableObject {
+class UserPreference: ObservableObject {
     @Published var headerText: [String] //should look into making this an attributed string
     @Published var buttonText: [String] //should look into making each element of this array an attributed string
     //no need for button number, as buttons will be generated for each button text
@@ -21,30 +21,33 @@ class Preference: ObservableObject {
         self.selected = selected
     }
     
+    init(headerText: [String], buttonText: [String], imageName: [String]) {
+        self.headerText = headerText
+        self.buttonText = buttonText
+        self.imageName = imageName
+    }
+    
     struct PreferencesSelected {
         /// String chosen here over Bool because Firebase seraching might be more complex for true or false values
         /// To display preferred restaurants, we apply filters. Default values indicate that no filters will be applied
-        var isHalaal: String = "NO"
-        var haram: String = "NO"
-        var pork: String = "NO"
-        var vegan: String = "NO"
-        var vegetarian: String = "NO"
-        var lactose: String = "NO"
-        var outdoor: String = "NO"
-        var wineTasting: String = "NO"
-        var wineFarms: String = "NO"
+        var isHalaal: Bool = false
+        var haram: Bool = false
+        var pork: Bool = false
+        var vegan: Bool = false
+        var vegetarian: Bool = false
+        var lactose: Bool = false
+        var outdoor: Bool = false
+        var wineTasting: Bool = false
+        var wineFarms: Bool = false
         var authenticRestaurants: AuthenticRestaurant?
         
         struct AuthenticRestaurant {
-            var african: (String, String) = ("African", "NO") //when searching Firebase, remember to be case-agnostic
-            var italian: (String, String) = ("Italian", "NO")
-            var greek: (String, String) = ("Greek", "NO")
-            var chinese: (String, String) = ("Chinese", "NO")
-            var thai: (String, String) = ("Thai", "NO")
+            var african: (String, Bool) = ("African", true) //when searching Firebase, remember to be case-agnostic
+            var italian: (String, Bool) = ("Italian", true)
+            var greek: (String, Bool) = ("Greek", true)
+            var chinese: (String, Bool) = ("Chinese", true)
+            var thai: (String, Bool) = ("Thai", true)
         }
     }
 }
 
-class PreferenceStore: ObservableObject {
-    @Published var preference : Preference?
-}
