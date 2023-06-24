5d12b66470d26d528da201eff687203d9b441886
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-06-24 10:10:52 +0200

    removed dynamicLookup subscript

--- Duke/Scaffold/UserPreferenceStore.swift
+++ Duke/Scaffold/UserPreferenceStore.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-@dynamicMemberLookup
+//  MARK: DynamicMemberLookup Implementation @dynamicMemberLookup
 class UserPreference: ObservableObject {
     //should use MVVM here
     //this class should be the model, with instances of this broadcasting (publishing) user selected data
@@ -16,42 +16,32 @@ class UserPreference: ObservableObject {
     //headerText will be changed into String values
     //use of Dictionary values looking likely, but this will be determined later
     //some properties to be moved to UserPreferenceModel https://paulallies.medium.com/swiftui-mvvm-a1e7a18f4f03
-    var id: String = UUID().uuidString //should be set to same as authid from Firebase
-    var isHalaal: Bool = false
-    var haram: Bool = false
-    var pork: Bool = false
-    var vegan: Bool = false
-    var vegetarian: Bool = false
-    var lactose: Bool = false
-    var outdoor: Bool = false
-    var wineTasting: Bool = false
-    var wineFarms: Bool = false
+    @Published var id: String = UUID().uuidString //should be set to same as authid from Firebase
+    @Published var halaal: Bool = false
+    @Published var haram: Bool = false
+    @Published var pork: Bool = false
+    @Published var vegan: Bool = false
+    @Published var vegetarian: Bool = false
+    @Published var lactose: Bool = false
+    @Published var outdoor: Bool = false
+    @Published var wineTasting: Bool = false
+    @Published var wineFarms: Bool = false
     
     ///authentic experiences preferences
-    var african: Bool = true
-    var italian: Bool = true
-    var greek: Bool = true
-    var chinese: Bool = true
-    var thai: Bool = true
+    @Published var african: Bool = true
+    @Published var italian: Bool = true
+    @Published var greek: Bool = true
+    @Published var chinese: Bool = true
+    @Published var thai: Bool = true
     
+    //Binding to buttons maybe
     
-    
-    init(headerText: [String], buttonText: [String], imageName: [String]) {
-        self.headerText = headerText
-        self.buttonText = buttonText
-        self.imageName = imageName
-    }
-    
-    init(headerText: [String] = [], buttonText: [String] = [], imageName: [String] = [],
-             isHalaal: Bool = false, haram: Bool = false, pork: Bool = false, vegan: Bool = false,
+    init(isHalaal: Bool = false, haram: Bool = false, pork: Bool = false, vegan: Bool = false,
              vegetarian: Bool = false, lactose: Bool = false, outdoor: Bool = false,
              wineTasting: Bool = false, wineFarms: Bool = false, african: Bool = true,
              italian: Bool = true, greek: Bool = true, chinese: Bool = true, thai: Bool = true) {
             
-            self.headerText = headerText
-            self.buttonText = buttonText
-            self.imageName = imageName
-            self.isHalaal = isHalaal
+            self.halaal = isHalaal
             self.haram = haram
             self.pork = pork
             self.vegan = vegan
@@ -66,38 +56,41 @@ class UserPreference: ObservableObject {
             self.chinese = chinese
             self.thai = thai
         }
+    
+    /*
     ///subscript may not have been needed to be done in this way. Instead of creating our own Binding retunred from the subscript, we were supposed to pass in a UserPreference object as is to ParallaxView, then hook into the bindings provided by the StateObject on its @Published properties
     ///-- Route taken in this case assumed StateObject values will change in-between ParallaxView swipes offscren, hence the need to create custom subscript returning custom bindings to our properties by string name
     ///--However, this implmentation was needed such that ParaalxView can be able initialize it's buttons based on its buttonText array input and creating bindings to them on the fly
     ///The unintended result of this approach is that we have to explictly initalize the StateObject, which has performance and functionality side-effects we may not anticipate
     subscript(dynamicMember key: String) -> Binding<Bool> {
-            Binding<Bool>(
-                get: {
-                    return self.value(for: key)
-                },
-                set: { newValue in
-                    self.setValue(newValue, for: key)
-                }
-            )
-        }
-        
-        private func value(for key: String) -> Bool {
-            return Mirror(reflecting: self).children
-                .compactMap { $0 as? (label: String?, value: Bool) }
-                .first { $0.label == key }?.value ?? false
-        }
-        
-        private func setValue(_ newValue: Bool, for key: String) {
-            let mirror = Mirror(reflecting: self)
-            guard var target = mirror.children
-                .first(where: {
-                    ($0.label ?? "") == key
-                })?.value as? Published<Bool> else {
-                return
+        Binding<Bool>(
+            get: {
+                return self.value(for: key)
+            },
+            set: { newValue in
+                self.setValue(newValue, for: key)
             }
-            
-            target = Published<Bool>(wrappedValue: newValue)
+        )
+    }
+    
+    private func value(for key: String) -> Bool {
+        return Mirror(reflecting: self).children
+            .compactMap { $0 as? (label: String?, value: Bool) }
+            .first { $0.label == key }?.value ?? false
+    }
+    
+    private func setValue(_ newValue: Bool, for key: String) {
+        let mirror = Mirror(reflecting: self)
+        guard var target = mirror.children
+            .first(where: {
+                ($0.label ?? "") == key
+            })?.value as? Published<Bool> else {
+            return
         }
+        
+        target = Published<Bool>(wrappedValue: newValue)
+    }
+     */
     
 }
 
