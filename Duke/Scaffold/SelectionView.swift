f41035d741b860a481931f5a152e25c52fde02e1
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-06-27 09:58:56 +0200

    New SelectionView to host the Parallax loops

---
+++ Duke/Scaffold/SelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectionView.swift
+//  Duke
+//
+//  Created by Simbarashe Dombodzvuku on 6/27/23.
+//
+
+import SwiftUI
+
+struct SelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectionView()
+    }
+}
