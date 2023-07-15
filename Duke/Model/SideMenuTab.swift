610ac261d90b9dbf0380cfaed0dcd771934c899d
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-07-15 19:00:10 +0200

    ResponsiveView and ScreenProperties added

--- Duke/Model/SideMenuTab.swift
+++ Duke/Model/SideMenuTab.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+//Tab enum model, raw value is image file name in assets
+enum SideMenuTab: String, CaseIterable {
+    case dashboard = "Dashboard"
+    case transaction = "Transaction"
+    case task = "Task"
+    case documents = "Documents"
+    case store  = "Store"
+    case notifications = "Notifications"
+    case profile = "Profile"
+    case settings = "Settings"
+}
---
+++ Duke/ResponsiveView/ScreenProperties.swift
@@ -0,0 +1,16 @@
+//
+//  ScreenProperties.swift
+//  Duke
+//
+//  Created by Simbarashe Dombodzvuku on 7/15/23.
+//
+
+import Foundation
+
+struct Properties {
+    var isLandscape: Bool
+    var isiPad: Bool
+    var isSplit: Bool
+    var size: CGSize
+}
+
