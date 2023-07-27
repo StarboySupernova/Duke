c727933034fb6115ca08030b168960f65f3cb783
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-07-27 15:26:38 +0200

    Moved the functionality of LoginContainer to SignInControllerView, thereby invalidating the former. This configuration is untested with Firebase logins

--- Duke/Scaffold/SignInControllerView.swift
+++ Duke/Scaffold/SignInControllerView.swift
@@ -13,45 +13,61 @@ struct SignInControllerView: View {
     @State private var selection = 0
     
     var body: some View {
-        VStack(spacing: 0) {
-            // MARK: Segmented Control
-            SegmentedControl(selection: $selection) //should use this also inside tabview
-            
-            // MARK: Nested Views
-            HStack(spacing: 12) {
-                if selection == 0 {
-                    //pass selection as a binding into these views
-                    LoginView(selection: $selection)
-                        .transition(.offset(x: -getRect().width))
-                } else {
-                    RegistrationView(selection: $selection)
-                        .transition(.offset(x: getRect().width))
+        if userLoginVM.sign_in_status { //show Posts and ProfileView. Possibly with BottomSheetView
+            VStack(spacing: 0) {
+                HStack(spacing: 12) {
+                    if selection == 0 {
+                        PostsView()
+                    } else {
+                        ProfileView()
+                    }
                 }
+                .padding(.vertical, 20)
+                
+                // MARK: Segmented Control
+                SegmentedControl(selection: $selection)
             }
-            .padding(.vertical, 20)
-        
-        }
-        .backgroundBlur(radius: 25, opaque: true)
-        .background(Image("Background"))
-        .background(Color.bottomSheetBackground)
-        .background(.ultraThinMaterial)
-        .clipShape(RoundedRectangle(cornerRadius: 44))
-        .innerShadow(shape: RoundedRectangle(cornerRadius: 44), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1 - bottomSheetTranslationProrated)
-        .overlay {
-            // MARK: Bottom Sheet Separator
-            Divider()
-                .blendMode(.overlay)
-                .background(Color.bottomSheetBorderTop)
-                .frame(maxHeight: .infinity, alignment: .top)
-                .clipShape(RoundedRectangle(cornerRadius: 44))
-        }
-        .overlay {
-            // MARK: Drag Indicator
-            RoundedRectangle(cornerRadius: 10)
-                .fill(.black.opacity(0.3))
-                .frame(width: 48, height: 5)
-                .frame(height: 20)
-                .frame(maxHeight: .infinity, alignment: .top)
+        } else {
+            VStack(spacing: 0) {
+                // MARK: Segmented Control
+                SegmentedControl(selection: $selection) //should use this also inside tabview
+                
+                // MARK: Nested Views
+                HStack(spacing: 12) {
+                    if selection == 0 {
+                        //pass selection as a binding into these views
+                        LoginView(selection: $selection)
+                            .transition(.offset(x: -getRect().width))
+                    } else {
+                        RegistrationView(selection: $selection)
+                            .transition(.offset(x: getRect().width))
+                    }
+                }
+                .padding(.vertical, 20)
+                
+            }
+            .backgroundBlur(radius: 25, opaque: true)
+            .background(Image("Background"))
+            .background(Color.bottomSheetBackground)
+            .background(.ultraThinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 44))
+            .innerShadow(shape: RoundedRectangle(cornerRadius: 44), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1 - bottomSheetTranslationProrated)
+            .overlay {
+                // MARK: Bottom Sheet Separator
+                Divider()
+                    .blendMode(.overlay)
+                    .background(Color.bottomSheetBorderTop)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .clipShape(RoundedRectangle(cornerRadius: 44))
+            }
+            .overlay {
+                // MARK: Drag Indicator
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.black.opacity(0.3))
+                    .frame(width: 48, height: 5)
+                    .frame(height: 20)
+                    .frame(maxHeight: .infinity, alignment: .top)
+            }
         }
     }
 }
@@ -59,7 +75,7 @@ struct SignInControllerView: View {
 struct SignInControllerView_Previews: PreviewProvider {
     static var previews: some View {
         SignInControllerView()
-            //.background(Color.background)
+        //.background(Color.background)
             .preferredColorScheme(.dark)
             .environmentObject(UserViewModel())
     }
