ec2590f93b07bd4c568c070927182ac1e299bcbf
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-08-13 19:24:36 +0200

    Wholesale changes to SideBar. Now modeled after SideMenu

--- Duke/ResponsiveView/SideBar.swift
+++ Duke/ResponsiveView/SideBar.swift
@@ -8,63 +8,162 @@
 import SwiftUI
 
 struct SideBar: View {
-    var prop: Properties
-    @Binding var currentTab: SideMenuTab
+    var prop: Properties //this should possibly be used in-view, else we can remove
+    @State var isDarkMode = false
+    @Binding var selectedMenu: SelectedMenu
+    
+    //should look up scroll solution implemeted earlier in DEMT
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            VStack {
-                Label {
-                    Text("your profile")
-                        .fontWeight(.semibold)
-                } icon: {
-                    Image("sampleprofileimage")
-                        .resizedToFill(width: 50, height: 50)
-                        .clipShape(RoundedCorner(radius: 20, corners: [.topRight, .bottomLeft]))
+        VStack(spacing: 0) {
+            HStack {
+                Image(systemName: "person.fill")
+                    .padding(12)
+                    .background(.white.opacity(0.2))
+                    .mask(Circle())
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Simbarashe Dombodzvuku") //should come from saved name stored in db
+                    Text("UI Designer")
+                        .font(.subheadline)
+                        .opacity(0.7)
                 }
-                .padding(.vertical, 20)
-                .padding(.bottom, 15)
-                
-                Rectangle()
-                    .fill(.white.opacity(0.1))
-                    .frame(height: 1)
-                    .padding(.horizontal, -15)
-                
-                ForEach(SideMenuTab.allCases, id: \.rawValue) { tab in
-                    Button {
-                        withAnimation(.easeInOut) {
-                            currentTab = tab
-                        }
-                    } label: {
-                        TitleCard(isActive: tab == currentTab, headingText: tab.rawValue, imageName: "")
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-                    .padding(.top, .medium)
-                }
+                Spacer()
             }
-            .padding(15)
+            .padding()
+            
+            Text("BROWSE")
+                .font(.subheadline).bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 24)
+                .padding(.top, 40)
+                .opacity(0.7)
+            
+            browse
+            
+            Text("Create")
+                .font(.subheadline).bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 24)
+                .padding(.top, 40)
+                .opacity(0.7)
+            
+            create
+            
+            Spacer()
+            
+            HStack(spacing: 14) {
+                menuItems3[0].icon.view()
+                    .frame(width: 32, height: 32)
+                    .opacity(0.6)
+                    .onChange(of: isDarkMode) { newValue in
+                        if newValue {
+                            try? menuItems3[0].icon.setInput("active", value: true)
+                        } else {
+                            try? menuItems3[0].icon.setInput("active", value: false)
+                        }
+                    }
+                Text(menuItems3[0].text)
+                
+                Toggle("", isOn: $isDarkMode)
+            }
+            .font(.headline)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(12)
+            .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            .padding(8)
         }
-        .frame(width: 220)
-        .backgroundBlur(radius: 25, opaque: true)
-        .background(Color.background)
-        .background(.ultraThinMaterial)
-        .clipShape(RoundedCorner(radius: 20, corners: [.topRight, .bottomRight]))
-        .overlay {
-            // MARK: Drag Indicator
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.offWhite.opacity(0.9))
-                .frame(width: 20, height: 1)
-                .frame(height: 20)
-                .frame(maxHeight: .infinity, alignment: .top)
-        }
+        .foregroundColor(.white)
+        .frame(maxWidth: 288, maxHeight: .infinity)
+        .background(Color(hex: "17203A"))
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color(hex: "17203A").opacity(0.3), radius: 40, x: 0, y: 20)
+        .innerShadow(shape:RoundedRectangle(cornerRadius: 30, style: .continuous), color: Color.bottomSheetBorderMiddle, lineWidth: 2, offsetX: 0, offsetY: 2, blur: 0, blendMode: .overlay, opacity: 1)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .innerShadow(shape:RoundedRectangle(cornerRadius: 30, style: .continuous), color: Color.bottomSheetBorderMiddle, lineWidth: 2, offsetX: 0, offsetY: 2, blur: 0, blendMode: .overlay, opacity: 1)
+
     }
     
-}
+    var browse: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            ForEach(menuItems) { item in
+                Rectangle()
+                    .frame(height: 1)
+                    .opacity(0.1)
+                    .padding(.horizontal, 16)
+                
+                HStack(spacing: 14) {
+                    item.icon.view()
+                        .frame(width: 32, height: 32)
+                        .opacity(0.6)
+                    Text(item.text)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(12)
+                .background(
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .fill(.blue)
+                        .frame(maxWidth: selectedMenu == item.menu ? .infinity : 0)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                )
+                .background(Color("Background 2"))
+                .onTapGesture {
+                    withAnimation(.timingCurve(0.2, 0.8, 0.2, 1)) {
+                        selectedMenu = item.menu
+                    }
+                    try? item.icon.setInput("active", value: true)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        try? item.icon.setInput("active", value: false)
+                    }
+                }
+            }
+        }
+        .font(.headline)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(8)
+    }
+    
+    var create: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            ForEach(menuItems2) { item in
+                Rectangle()
+                    .frame(height: 1)
+                    .opacity(0.1)
+                    .padding(.horizontal, 16)
+                
+                HStack(spacing: 14) {
+                    item.icon.view()
+                        .frame(width: 32, height: 32)
+                        .opacity(0.6)
+                    Text(item.text)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(12)
+                .background(
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .fill(.blue)
+                        .frame(maxWidth: selectedMenu == item.menu ? .infinity : 0)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                )
+                .background(Color("Background 2"))
+                .onTapGesture {
+                    withAnimation(.timingCurve(0.2, 0.8, 0.2, 1)) {
+                        selectedMenu = item.menu
+                    }
+                    try? item.icon.setInput("active", value: true)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        try? item.icon.setInput("active", value: false)
+                    }
+                }
+            }
+        }
+        .font(.headline)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(8)
+    }}
 
 struct SideBar_Previews: PreviewProvider {
     static var previews: some View {
         //BookingView()
-        HomeView(showSideBar: .constant(false))
+        HomeView(showSideBar: .constant(false), selectedMenu: .constant(.home))
             .environmentObject(HomeViewModel())
             .preferredColorScheme(.dark)
     }
