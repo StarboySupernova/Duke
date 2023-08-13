87e19a91894e9303eda815c0b6aab26794cf9095
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-08-13 19:19:53 +0200

    StraddleScreen environmentObject

--- Duke/DukeApp.swift
+++ Duke/DukeApp.swift
@@ -16,6 +16,7 @@ import FirebaseStorage
 struct DukeApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @Environment(\.colorScheme) var colorScheme
+    @StateObject private var straddleScreen: StraddleScreen =  StraddleScreen() //will be used as nevironment variable to control the postioning of VerticalTabBar
 
     init() {
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = .black
@@ -27,7 +28,8 @@ struct DukeApp: App {
         WindowGroup {
             ContentView()
                 //.modifier(DarkModeViewModifier())
-                .environmentObject(HomeViewModel())            
+                .environmentObject(HomeViewModel())
+                .environmentObject(straddleScreen)
         }
     }
 }
