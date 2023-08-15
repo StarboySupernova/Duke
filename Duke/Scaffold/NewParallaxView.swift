061bcbf9bf8e865522eb7de77f31f198b9e8252d
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-08-15 05:02:12 +0200

    parallax UI augmentation attempt

---
+++ Duke/Scaffold/NewParallaxView.swift
@@ -0,0 +1,20 @@
+//
+//  NewParallaxView.swift
+//  Duke
+//
+//  Created by Simbarashe Dombodzvuku on 8/15/23.
+//
+
+import SwiftUI
+
+struct NewParallaxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewParallaxView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewParallaxView()
+    }
+}
