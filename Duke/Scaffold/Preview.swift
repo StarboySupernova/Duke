5bdc72a885698211fb05021af194606456b7d331
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-09-26 14:50:55 +0200

    code comments on VideoPlayer functionality

--- Duke/Scaffold/Preview.swift
+++ Duke/Scaffold/Preview.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 import AVFoundation
 
+// A SwiftUI View that represents a live camera preview using AVCaptureSession
 struct Preview: UIViewControllerRepresentable {
     let previewLayer: AVCaptureVideoPreviewLayer
     let gravity: AVLayerVideoGravity
@@ -21,19 +22,25 @@ struct Preview: UIViewControllerRepresentable {
         self.previewLayer = AVCaptureVideoPreviewLayer(session: session)
     }
 
+    // Create the UIViewController to host the AVCaptureVideoPreviewLayer
     func makeUIViewController(context: Context) -> UIViewController {
         let viewController = UIViewController()
         return viewController
     }
 
+    // Update the UIViewController when the SwiftUI view changes
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         previewLayer.videoGravity = gravity
+        // Add the AVCaptureVideoPreviewLayer as a sublayer to the UIViewController's view
         uiViewController.view.layer.addSublayer(previewLayer)
         
+        // Ensure the preview layer fills the entire view
         previewLayer.frame = uiViewController.view.bounds
     }
 
+    // Cleanup when the SwiftUI view is dismantled
     func dismantleUIViewController(_ uiViewController: UIViewController, coordinator: ()) {
+        // Remove the AVCaptureVideoPreviewLayer from its superlayer when no longer needed
         previewLayer.removeFromSuperlayer()
     }
 }
--- Duke/Scaffold/VideoContentViewModel.swift
+++ Duke/Scaffold/VideoContentViewModel.swift
@@ -21,12 +21,13 @@ class VideoContentViewModel: NSObject, ObservableObject {
         Task(priority: .background) {
             switch await AuthorizationChecker.checkCaptureAuthorizationStatus() {
             case .permitted:
+                // Add video input and output, then start the session asynchronously.
                 try session
                     .addMovieInput()
                     .addMovieFileOutput()
                     .startRunning() //startRunning is a thread-blocking method that can impair responsiveness if run on the main thread. This is why the background priority is given to Task.
                 
-                DispatchQueue.main.async {
+                DispatchQueue.main.async { // Update the preview on the main thread.
                     self.preview = Preview(with: self.session, gravity: .resizeAspectFill)
                 }
                 
@@ -48,13 +49,14 @@ class VideoContentViewModel: NSObject, ObservableObject {
               print("Cannot access local file domain")
               return
           }
-          
+        #warning("Generate a unique filename for the recorded video based on userID, not just at random such as the present implementation")
           let fileName = UUID().uuidString
 
           let filePath = directoryPath
               .appendingPathComponent(fileName)
               .appendingPathExtension("mp4")
           
+        // Start recording to the specified file path.
           output.startRecording(to: filePath, recordingDelegate: self)
       }
       
@@ -68,11 +70,12 @@ class VideoContentViewModel: NSObject, ObservableObject {
       }
   }
 
+// Adding conformance to AVCaptureFileOutputRecordingDelegate for recording callbacks.
   extension VideoContentViewModel: AVCaptureFileOutputRecordingDelegate {
       func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
           print("Video record is finished!")
           
-          // ADDED
+          //Requesting authorization to save the video to the photo library.
           Task {
               guard
                   case .authorized = await PHPhotoLibrary.requestAuthorization(for: .addOnly)
@@ -81,6 +84,7 @@ class VideoContentViewModel: NSObject, ObservableObject {
                   return
               }
               
+              #warning("Should come back and catch errors defined & emitted here")
               let library = PHPhotoLibrary.shared()
               let album = try getAlbum(name: "YOUR_ALBUM_NAME", in: library)
               try await add(video: outputFileURL, to: album, library)
@@ -88,6 +92,7 @@ class VideoContentViewModel: NSObject, ObservableObject {
       }
   }
 
+// Extension for helper functions related to saving to the photo library
 extension VideoContentViewModel {
     /// Add the video to the app's album roll
     func add(video path: URL, to album: PHAssetCollection, _ photoLibrary: PHPhotoLibrary) async throws -> Void {
@@ -107,7 +112,11 @@ extension VideoContentViewModel {
     }
     
     func getAlbum(name: String, in photoLibrary: PHPhotoLibrary) throws -> PHAssetCollection {
+        // Create a new instance of PHFetchOptions, which is used to specify options for fetching assets or collections from the Photos library.
         let fetchOptions = PHFetchOptions()
+
+        // Set a predicate for filtering the fetched results based on a condition.
+        // In this case, we are creating an NSPredicate that filters based on the "title" property being equal to the provided "name".
         fetchOptions.predicate = NSPredicate(format: "title = %@", name)
         
         let collection = PHAssetCollection.fetchAssetCollections(
@@ -117,6 +126,8 @@ extension VideoContentViewModel {
         if let album = collection.firstObject {
             return album
         } else {
+            #warning("should handle potential errors thrown here")
+            // If the album doesn't exist, create it.
             try createAlbum(name: name, in: photoLibrary)
             return try getAlbum(name: name, in: photoLibrary)
         }
