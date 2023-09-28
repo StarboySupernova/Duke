86d8ebab02228470461b8d5cc9e4e615e5fe597b
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-09-28 05:09:10 +0200

    https://github.com/enebin/Aespa/issues/21#issuecomment-1738318647_

--- Duke/Scaffold/VideoContentViewModel.swift
+++ Duke/Scaffold/VideoContentViewModel.swift
@@ -5,140 +5,109 @@
 //  Created by Simbarashe Dombodzvuku on 9/23/23.
 //
 
+import Combine
+import SwiftUI
 import Foundation
-import AVFoundation
-import Photos
 
-class VideoContentViewModel: NSObject, ObservableObject {
-    let session: AVCaptureSession
-    @Published var preview: Preview?
+import Aespa
+
+class VideoContentViewModel: ObservableObject {
+    let aespaSession: AespaSession
     
-    override init() {
-        self.session = AVCaptureSession()
+    var preview: some View {
+        return aespaSession.interactivePreview()
         
-        super.init()
+        // Or you can give some options
+//        let option = InteractivePreviewOption(enableShowingCrosshair: false)
+//        return aespaSession.interactivePreview(option: option)
+    }
+    
+    private var subscription = Set<AnyCancellable>()
+    
+    @Published var videoAlbumCover: Image?
+    @Published var photoAlbumCover: Image?
+    
+    @Published var videoFiles: [VideoAsset] = []
+    @Published var photoFiles: [PhotoAsset] = []
+    
+    init() {
+        var option = AespaOption(albumName: "Aespa-Demo-App")
+        self.aespaSession = Aespa.session(with: option)
         
-        Task(priority: .background) {
-            switch await AuthorizationChecker.checkCaptureAuthorizationStatus() {
-            case .permitted:
-                // Add video input and output, then start the session asynchronously.
-                try session
-                    .addMovieInput()
-                    .addMovieFileOutput()
-                    .startRunning() //startRunning is a thread-blocking method that can impair responsiveness if run on the main thread. This is why the background priority is given to Task.
-                
-                DispatchQueue.main.async { // Update the preview on the main thread.
-                    self.preview = Preview(with: self.session, gravity: .resizeAspectFill)
+        // Common setting
+        aespaSession
+            .focus(mode: .continuousAutoFocus)
+            .changeMonitoring(enabled: true)
+            .orientation(to: .portrait)
+            .quality(to: .high)
+            .custom(WideColorCameraTuner()) { result in
+                if case .failure(let error) = result {
+                    print("Error: ", error)
                 }
-                
-            case .notPermitted:
-                break
             }
-        }
-    }
-    
-    func startRecording() {
-          guard let output = session.movieFileOutput else {
-              print("Cannot find movie file output")
-              return
-          }
-          
-          guard
-              let directoryPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-          else {
-              print("Cannot access local file domain")
-              return
-          }
-        #warning("Generate a unique filename for the recorded video based on userID, not just at random such as the present implementation")
-          let fileName = UUID().uuidString
-
-          let filePath = directoryPath
-              .appendingPathComponent(fileName)
-              .appendingPathExtension("mp4")
-          
-        // Start recording to the specified file path.
-          output.startRecording(to: filePath, recordingDelegate: self)
-      }
-      
-      func stopRecording() {
-          guard let output = session.movieFileOutput else {
-              print("Cannot find movie file output")
-              return
-          }
-          
-          output.stopRecording()
-      }
-  }
-
-// Adding conformance to AVCaptureFileOutputRecordingDelegate for recording callbacks.
-  extension VideoContentViewModel: AVCaptureFileOutputRecordingDelegate {
-      func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
-          print("Video record is finished!")
-          
-          //Requesting authorization to save the video to the photo library.
-          Task {
-              guard
-                  case .authorized = await PHPhotoLibrary.requestAuthorization(for: .addOnly)
-              else {
-                  print("Cannot gain authorization")
-                  return
-              }
-              
-              #warning("Should come back and catch errors defined & emitted here")
-              let library = PHPhotoLibrary.shared()
-              let album = try getAlbum(name: "YOUR_ALBUM_NAME", in: library)
-              try await add(video: outputFileURL, to: album, library)
-          }
-      }
-  }
-
-// Extension for helper functions related to saving to the photo library
-extension VideoContentViewModel {
-    /// Add the video to the app's album roll
-    func add(video path: URL, to album: PHAssetCollection, _ photoLibrary: PHPhotoLibrary) async throws -> Void {
-        return try await photoLibrary.performChanges {
-            guard
-                let assetChangeRequest = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: path),
-                let placeholder = assetChangeRequest.placeholderForCreatedAsset,
-                let albumChangeRequest = PHAssetCollectionChangeRequest(for: album)
-            else {
-                print("Cannot access to album")
-                return
-            }
-            
-            let enumeration = NSArray(object: placeholder)
-            albumChangeRequest.addAssets(enumeration)
-        }
-    }
-    
-    func getAlbum(name: String, in photoLibrary: PHPhotoLibrary) throws -> PHAssetCollection {
-        // Create a new instance of PHFetchOptions, which is used to specify options for fetching assets or collections from the Photos library.
-        let fetchOptions = PHFetchOptions()
-
-        // Set a predicate for filtering the fetched results based on a condition.
-        // In this case, we are creating an NSPredicate that filters based on the "title" property being equal to the provided "name".
-        fetchOptions.predicate = NSPredicate(format: "title = %@", name)
         
-        let collection = PHAssetCollection.fetchAssetCollections(
-            with: .album, subtype: .any, options: fetchOptions
-        )
-
-        if let album = collection.firstObject {
-            return album
-        } else {
-            #warning("should handle potential errors thrown here")
-            // If the album doesn't exist, create it.
-            try createAlbum(name: name, in: photoLibrary)
-            return try getAlbum(name: name, in: photoLibrary)
+        // Photo-only setting
+        aespaSession
+            .flashMode(to: .on)
+            .redEyeReduction(enabled: true)
+        
+        // Video-only setting
+        aespaSession
+            .mute()
+            .stabilization(mode: .auto)
+        
+        // Prepare video album cover
+        aespaSession.videoFilePublisher
+            .receive(on: DispatchQueue.main)
+            .map { result -> Image? in
+                if case .success(let file) = result {
+                    return file.thumbnailImage
+                } else {
+                    return nil
+                }
+            }
+            .assign(to: \.videoAlbumCover, on: self)
+            .store(in: &subscription)
+        
+        // Prepare photo album cover
+        aespaSession.photoFilePublisher
+            .receive(on: DispatchQueue.main)
+            .map { result -> Image? in
+                if case .success(let file) = result {
+                    return file.thumbnailImage
+                } else {
+                    return nil
+                }
+            }
+            .assign(to: \.photoAlbumCover, on: self)
+            .store(in: &subscription)
+    }
+    
+    func fetchVideoFiles() {
+        // File fetching task can cause low reponsiveness when called from main thread
+        Task(priority: .utility) {
+            let fetchedFiles = await aespaSession.fetchVideoFiles()
+            DispatchQueue.main.async { self.videoFiles = fetchedFiles }
         }
     }
     
-    func createAlbum(name: String, in photoLibrary: PHPhotoLibrary) throws {
-        try photoLibrary.performChangesAndWait {
-            PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: name)
+    func fetchPhotoFiles() {
+        // File fetching task can cause low reponsiveness when called from main thread
+        Task(priority: .utility) {
+            let fetchedFiles = await aespaSession.fetchPhotoFiles()
+            DispatchQueue.main.async { self.photoFiles = fetchedFiles }
         }
     }
 }
 
+
+extension VideoContentViewModel {
+    // Example for using custom session tuner
+    struct WideColorCameraTuner: AespaSessionTuning {
+        func tune<T>(_ session: T) throws where T : AespaCoreSessionRepresentable {
+            session.avCaptureSession.automaticallyConfiguresCaptureDeviceForWideColor = true
+        }
+    }
+}
 
 
